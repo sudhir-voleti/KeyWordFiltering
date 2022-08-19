5f16eb6e1a978a83b4a821438b955f858963d2f8
Shivani Srivastava <shivani141296@gmail.com>
2022-08-19 13:34:12 +0530

    Update dependency.R

--- dependency.R
+++ dependency.R
@@ -1,1 +1,16 @@
+suppressPackageStartupMessages({
 
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("tidyr")) {install.packages("tidyr")}
+if(!require("DT")) {install.packages("DT")}
+if(!require("stringr")) {install.packages("stringr")}
+if(!require("quanteda")) {install.packages("quanteda")}
+if(!require("tidytext")) {install.packages("tidytext")}
+
+library(shiny)
+library(tidyr)
+library(DT)
+library(stringr)
+library(quanteda)
+library(tidytext)
+})
