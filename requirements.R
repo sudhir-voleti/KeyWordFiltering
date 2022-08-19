d4b10d40659ad6bbb540583b187c4fdb92bd6391
Shivani Srivastava <shivani141296@gmail.com>
2022-08-19 13:49:47 +0530

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -6,11 +6,14 @@ if(!require("DT")) {install.packages("DT")}
 if(!require("stringr")) {install.packages("stringr")}
 if(!require("quanteda")) {install.packages("quanteda")}
 if(!require("tidytext")) {install.packages("tidytext")}
-
+if(!require("purrr")) {install.packages("purrr")}  
+if(!require("dplyr")) {install.packages("dplyr")}
+  
 library(shiny)
 library(tidyr)
 library(DT)
 library(stringr)
 library(quanteda)
 library(tidytext)
+library(dplyr)  
 })
