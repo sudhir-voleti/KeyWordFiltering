c5456db335573b2d75e87f600e692fb269607387
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-04-30 16:41:19 +0530

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,19 +1,22 @@
 suppressPackageStartupMessages({
 
 if(!require("shiny")) {install.packages("shiny")}
-if(!require("tidyr")) {install.packages("tidyr")}
+if(!require("tidyverse")) {install.packages("tidyverse")}
 if(!require("DT")) {install.packages("DT")}
 if(!require("stringr")) {install.packages("stringr")}
 if(!require("quanteda")) {install.packages("quanteda")}
 if(!require("tidytext")) {install.packages("tidytext")}
 if(!require("purrr")) {install.packages("purrr")}  
 if(!require("dplyr")) {install.packages("dplyr")}
+if(!require("pdftools")) {install.packages("pdftools")}
+if(!require("tools")) {install.packages("tools")}
   
 library(shiny)
-library(tidyr)
+library(tidyverse)
 library(DT)
 library(stringr)
 library(quanteda)
 library(tidytext)
-library(dplyr)  
+library(pdftools)
+library(tools)
 })
